f5160e641dd61d29a728fc07fc6cf0b3cfa513da
Викуся <@DESKTOP-634PAOE>
2023-10-23 02:48:03 +0300

    Добавьте файлы проекта.

---
+++ JSON/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+public class PublishingHouse
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Adress { get; set; }
+}
+
+public class BookData
+{
+    public int PublishingHouseId { get; set; }
+
+    public string Title { get; set; }
+
+    public PublishingHouse PublishingHouse { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Зчитати JSON-рядок з файлу (замініть шлях на відповідний)
+        string json = File.ReadAllText("C:\\Users\\Викуся\\source\\repos\\JSON\\JSON\\jsconfig1.json");
+
+        // Десеріалізувати JSON-рядок у масив об'єктів BookData
+        BookData[] books = JsonConvert.DeserializeObject<BookData[]>(json);
+
+        // Вивести дані на консоль
+        foreach (var book in books)
+        {
+            Console.WriteLine($"Title: {book.Title}");
+            Console.WriteLine($"Publishing House: {book.PublishingHouse.Name}");
+            Console.WriteLine($"Adress: {book.PublishingHouse.Adress}");
+            Console.WriteLine();
+        }
+    }
+}
